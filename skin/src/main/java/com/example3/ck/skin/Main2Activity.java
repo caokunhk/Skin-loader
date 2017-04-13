d056c7077c7223650ba485a400a5c36766f94e3a
caokun <caokunhk@163.com>
2017-04-13 18:20:49 +0800

    add feature-1 on feature-1 branch

--- skin/src/main/java/com/example3/ck/skin/Main2Activity.java
+++ skin/src/main/java/com/example3/ck/skin/Main2Activity.java
@@ -2,6 +2,8 @@ package com.example3.ck.skin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import cn.feng.skinpackage.R;
 
@@ -11,5 +13,14 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    /**
+     * feature-1
+     * display HelloWorld
+     * @param view
+     */
+    public void feature1(View view) {
+        Log.i("123","feature1");
     }
 }
